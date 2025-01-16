82cc4e6a62bc4350e78ceccc214a5b6196e4fae4
youngsun1124 <dbdudtjs@nate.com>
2025-01-16 09:07:21 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div  className = "gray-background">
+     <img src={logo} lat="log" />
+     <h2>
+        managemant system
+     </h2>
+    </div>
+  );
+}
+
+export default App;
