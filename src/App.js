9bef3b67d143e53eb2355d57758eb9ec3d27038b
youngsun1124 <dbdudtjs@nate.com>
2025-01-17 15:17:22 +0900

    변경 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
+ 
+const customers = [{ 
+  'id'    : 1,
+  'image' : 'https//placeing.com/64/64/any',
+  'name' : '김가군',
+  'birthday' : '731225',
+  'gender' : '남자',
+  'job' : '학생'
+},
+{ 
+  'id'    : 2,
+  'image' : 'https//placeing.com/64/64/any',
+  'name' : '김다비',
+  'birthday' : '731224',
+  'gender' : '여자',
+  'job' : '학생'
+},
+{ 
+  'id'    : 3,
+  'image' : 'https//placeing.com/64/64/any',
+  'name' : '김나군',
+  'birthday' : '7312254',
+  'gender' : '남자',
+  'job' : '학생'
+}
 
-function App() {
-  return (
-    <div  className = "gray-background">
-     <img src={logo} lat="log" />
-     <h2>
-        managemant system
-     </h2>
-    </div>
-  );
+]
+
+class App extends  Component{
+  render(){
+      return ( 
+         <div>
+          {
+            customers.map( c => {
+              return(
+                <Customer
+                  key={c.id}
+                  id={c.id} 
+                  image={c.image}
+                  name={c.name}
+                  gender={c.gender}
+                  job={c.job}
+                  birthday={c.birthday}
+                />
+              )
+           })
+          } 
+         </div>
+      );
+  }
 }
 
 export default App;
